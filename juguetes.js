6c781998ba1958a8fa97954556a6b58ecf95e0cb
matias9809 <matiasloiza@hotmail.es>
2023-01-15 17:33:49 -0300

    pagina de farmacia y juguetes(botones agregar y eliminar)

--- juguetes.js
+++ juguetes.js
@@ -2,36 +2,33 @@ const { createApp } = Vue
 createApp( {
     data(){
         return {
-            disponibles:[],
             tarjetas : [],
             categorias : [],
             valorDeBusqueda: "",
             chequeados: [],
             tarjetasFiltradas : [],
-            sin_datos:"looding",
-            carrito:[],
-            aux:[]
+            sin_datos:"looding"
         }
     },
     created(){
         fetch(`https://mindhub-xj03.onrender.com/api/petshop`)
             .then( respuesta => respuesta.json() )
             .then( datos => {
-                this.disponibles = datos.map(e=>{
-                    if(e.categoria=="farmacia"){
-                        return{
-                            _id:e._id,
-                            disponibles:e.disponibles
-
-                        }
+                this.tarjetas = datos.filter(e=>e.categoria=="jugueteria")
+                this.tarjetasFiltradas = this.tarjetas
+                this.categorias = [ ...new Set( this.tarjetas.map( tar => tar.precio ) ) ]
+/*                 this.categorias =this.tarjetas.foreach( tar => {
+                    if(tar.precio<=1500){
+                        categoria.push(1500)
                     }
-                })
-                this.tarjetas= datos.filter(e=>e.categoria=="farmacia")
-                this.categorias=datos.filter(e=>e.categoria=="farmacia")
-                this.tarjetasFiltradas = datos.filter(e=>e.categoria=="farmacia")
-                console.log(this.tarjetasFiltradas,this.tarjetas)
-                this.categorias = [ ...new Set( this.categorias.map( tar => tar.precio ) ) ]
-            } )
+                    else if(tar.precio<=2200&&tar.precio>1500){
+                        categoria.push(2200)
+                    }
+                    else if(tar.precio>2200){
+                        categoria.push(2201)
+                    }
+                } )  */
+                        } )
             .catch( )   
     },
     methods: {
@@ -41,19 +38,28 @@ createApp( {
                 this.tarjetasFiltradas = filtradoPorBusqueda
             }else{
                 let filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados.includes( eventos.precio ))
+/*                 let filtradosPorCheck = filtradoPorBusqueda.filter( eventos => {
+                    if(chequeados==1500)
+                    {this.chequeados.includes( eventos.precio<=chequeados )}
+                    else if(chequeados==2200){
+                        this.chequeados.includes( eventos.precio<=chequeados&&eventos>1500)
+                    }
+                    else if(chequeados>=2201){
+                        this.chequeados.includes( eventos.precio>=chequeados)
+                    } */
+/*                 }) */
                 this.tarjetasFiltradas = filtradosPorCheck 
             }
-        },
-    agregar: function(array){
-            if(array.disponibles==0){
-                alert("no hay stock disponible")
-            }
+        } ,
+        agregar: function(array){
             if(array.disponibles>0){
                 this.carrito=this.carrito.concat(array)
                 array.disponibles--
                 console.log(this.carrito)
             }
-
+            if(array.disponibles==0){
+                alert("no hay stock disponible")
+            }
         },
         eliminar: function(objeto){
             console.log("inicio")
@@ -78,18 +84,13 @@ createApp( {
                 this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
                 this.carrito=this.carrito.concat(this.aux)
                 console.log(this.carrito)}
-
                 else{
                     console.log("else")
                     this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
                     console.log(this.carrito)
                 }
-            }                
-            else if(this.carrito.length==0){
-                    alert("no hay productos que eliminar en el carrito ")
-                }
+            }
         }
-
     },
 
 } ).mount("#app")
