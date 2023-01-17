8576fa172c5da22a08e2d76244f84c43852221d7
Mauricio <mauri.f.mores@gmail.com>
2023-01-17 18:44:49 -0300

    arreglando tienda

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -2,113 +2,66 @@ const { createApp } = Vue
 createApp( {
     data(){
         return {
-            disponibles:[],
-            tarjetas : [],
-            categorias : [],
-            valorDeBusqueda: "",
-            chequeados: [],
-            tarjetasFiltradas : [],
-            carrito:[],
-            aux:[],
-            informacionDeTarjeta: "",
-            categoriasFiltradas: [],
-            categoriasOrdenadasMenorAMayor: []
+            todosLosProductos: [],
+            compras: [],
+            productos:[],
+            informacionDeTarjeta:{},
+            chequeados:[],
+            valorDeBusqueda:"",
         }
     },
     created(){
         fetch(`https://mindhub-xj03.onrender.com/api/petshop`)
             .then( respuesta => respuesta.json() )
             .then( datos => {
-                switch(document.getElementById("titulo").innerHTML){
-                    case "PATITAS FELICES | JUGUETES":{
-                        this.disponibles =[...new Set( datos.map(e=>{
-                            if(e.categoria=="jugueteria"){
-                                return{
-                                    _id:e._id,
-                                    disponibles:e.disponibles
-        
-                                }
-                            }
-                        }))]
-                        this.disponibles.shift()
-                        this.tarjetas= datos.filter(e=>e.categoria=="jugueteria")
-                        this.categorias=datos.filter(e=>e.categoria=="jugueteria")
-                        this.tarjetasFiltradas = datos.filter(e=>e.categoria=="jugueteria")
-                        this.categoriasFiltradas = [ ...new Set( this.categorias.map( tar => tar.precio ) ) ]
-                        this.categoriasOrdenadasMenorAMayor = this.categoriasFiltradas.sort((a, b) => a -b)
-                        break
-                    }
-                    case "PATITAS FELICES | FARMACIA":{
-                        this.disponibles =[...new Set( datos.map(e=>{
-                            if(e.categoria=="farmacia"){
-                        return{
-                            _id:e._id,
-                            disponibles:e.disponibles
-                                }
-                            }
-                        }))]
-                        this.tarjetas= datos.filter(e=>e.categoria=="farmacia")
-                        this.categorias=datos.filter(e=>e.categoria=="farmacia")
-                        this.tarjetasFiltradas = datos.filter(e=>e.categoria=="farmacia")
-                        this.categoriasFiltradas = [ ...new Set( this.categorias.map( tar => tar.precio ) ) ]
-                        this.categoriasOrdenadasMenorAMayor = this.categoriasFiltradas.sort((a, b) => a -b)
-                        break
-                    }
-                }
-
+                this.todosLosProductos=[...datos];
+                this.todosLosProductos.forEach(producto=>producto.ventas=0) 
+                if(localStorage.getItem("nuestrosProductos"))
+                this.todosLosProductos = JSON.parse(localStorage.getItem("nuestrosProductos"))
+                let nombrePagina=location.pathname;//ve pagina nombre
+                if (nombrePagina.includes("farmacia"))
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="farmacia");
+                else
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="jugueteria");
+                console.log(this.compras)
             } )
-            .catch( )   
-            JSON.parse(localStorage.getItem("carrito"))
+            .catch()
     },
     methods: {
-    filtroCruzado: function(){
-            let filtradoPorBusqueda = this.tarjetas.filter( eventos => eventos.producto.toLowerCase().includes( this.valorDeBusqueda.toLowerCase()))
+        filtroCruzado: function(){
+            let filtradoPorBusqueda = this.productos.filter( eventos => eventos.producto.toLowerCase().includes( this.valorDeBusqueda.toLowerCase()))
             if( this.chequeados.length === 0 ){
-                this.tarjetasFiltradas = filtradoPorBusqueda
+                this.productosFiltrados = filtradoPorBusqueda
             }else{
-                let filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados.includes( eventos.precio ))
-                this.tarjetasFiltradas = filtradosPorCheck 
+                let filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados( eventos.precio ))
+                this.productosFiltrados = filtradosPorCheck 
             }
         },
-    agregar: function(objeto){
-            if(objeto.disponibles==0){
-                alert("no hay stock disponible")
-            }
-            if(objeto.disponibles>0){
-                this.carrito=this.carrito.concat(objeto)
-                objeto.disponibles--
-                localStorage.setItem("carrito",JSON.stringify(this.carrito))
-            }
-
+        agregar: function(objeto){//crear v-if en disponibles y boton agregar colocar mensaje no hay displonibles
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles--;
+                producto.ventas++;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            console.log(this.compras)
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
         },
         eliminar: function(objeto){
-            let filtro=this.disponibles.find(e=>e._id==objeto._id)
-            if(this.carrito.some(e=>e._id==objeto._id)){
-                this.aux=this.carrito.filter(e=>e._id==objeto._id)
-                if(objeto.disponibles<filtro.disponibles){
-                    this.aux.shift()
-                    objeto.disponibles++
-                }
-                if(this.aux.length>0){
-                this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
-                this.carrito=this.carrito.concat(this.aux)
-                }
-
-                else{
-                    this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
-                }
-            }                
-            else if(this.carrito.length==0){
-                alert("no hay productos que eliminar en el carrito ")
-            }
-            else if(this.carrito.length>0){
-                alert("ese producto no se encuentra en el carrito")
-            }
-            localStorage.setItem("carrito",JSON.stringify(this.carrito))
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles++;
+                producto.ventas--;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
         },
         verMas: function(id){
-            this.tarjetas.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
-        }
-    },
+            this.todosLosProductos.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
+        },
+        removerCarrito:function(){
+        localStorage.removeItem("nuestrosProductos");
+        },
+    }
+    }).mount("#app")
 
-} ).mount("#app")
