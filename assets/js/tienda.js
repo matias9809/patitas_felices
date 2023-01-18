0f1bf462e5c0158b8acd153197e186cec7e9c57e
darthenis <emi.acevedo.letras@gmail.com>
2023-01-17 23:47:12 -0300

    update

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -6,7 +6,7 @@ createApp( {
             compras: [],
             productos:[],
             informacionDeTarjeta:{},
-            chequeados:[],
+            chequeados:"",
             valorDeBusqueda:"",
             todos:[],
         }
@@ -16,7 +16,7 @@ createApp( {
             .then( respuesta => respuesta.json() )
             .then( datos => {
                 this.todosLosProductos=[...datos];
-                
+
                 this.todosLosProductos.forEach(producto=>producto.ventas=0);
 
                 if(localStorage.getItem("nuestrosProductos"))
@@ -28,6 +28,8 @@ createApp( {
                 this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="jugueteria");
 
                 this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+
+                this.productosFiltrados = this.productos;
             } )
             .catch()
     },
@@ -37,7 +39,17 @@ createApp( {
             if( this.chequeados.length === 0 ){
                 this.productosFiltrados = filtradoPorBusqueda
             }else{
-                
+                let filtradosPorCheck
+                if(this.chequeados<=1500){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos =>this.chequeados>=eventos.precio)
+                }else if(this.chequeados==1501){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos => (this.chequeados<eventos.precio&&eventos.precio<=2000))
+                }
+                else if(this.chequeados==2001){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados<=eventos.precio)
+                    
+                }
+                this.productosFiltrados = filtradosPorCheck 
             }
         },
         agregar: function(objeto){//crear v-if en disponibles y boton agregar colocar mensaje no hay displonibles
@@ -62,14 +74,14 @@ createApp( {
         verMas: function(id){
             this.todosLosProductos.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
         },
-        removerCarrito:function(){
-        localStorage.removeItem("nuestrosProductos");
-        this.productos=this.todos;
-        this.compras=[];
-        console.log(this.productos)
-        },
+        // removerCarrito:function(){
+        // localStorage.removeItem("nuestrosProductos");
+        // this.productos=this.todos;
+        // this.compras=[];
+        // console.log(this.productos)
+        // },
     },
-    computed: {
+  /*   computed: {
 
         cargando(){
 
@@ -77,6 +89,6 @@ createApp( {
         
         }
 
-    }
+    } */
     }).mount("#app")
 
