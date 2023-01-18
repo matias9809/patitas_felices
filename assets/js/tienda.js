1d2a062052a968d86134991e652af134243d3211
darthenis <emi.acevedo.letras@gmail.com>
2023-01-18 07:34:43 -0300

    update tienda contacto

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -1,124 +1,182 @@
 const { createApp } = Vue
+
 createApp( {
     data(){
         return {
-            disponibles:[],
-            tarjetas : [],
-            categorias : [],
-            valorDeBusqueda: "",
-            chequeados: [],
-            tarjetasFiltradas : [],
-            carrito:[],
-            aux:[],
-            informacionDeTarjeta: "",
-            categoriasFiltradas: [],
-            categoriasOrdenadasMenorAMayor: [],
-            estaCargando: true
+            todosLosProductos: [],
+            compras: [],
+            productos:[],
+            informacionDeTarjeta:{},
+            chequeados:"",
+            valorDeBusqueda:"",
+            switchCheck: "",
+            todos:[],
+            estaCargando: true,
+            productosFiltrados : [],
+            perroOGtao: "",
+            data: {
+                nombre: "",
+                apellido: "",
+                telefono: "",
+                mascota: "",
+                otraMascota: "",
+                mensaje: ""
+            },
+            errores: {
+                nombre: "",
+                apellido: "",
+                telefono: "",
+                mascota: "",
+                otraMascota: "",
+                mensaje: "",
+
+            },
+            enviando: null
         }
     },
     created(){
         fetch(`https://mindhub-xj03.onrender.com/api/petshop`)
             .then( respuesta => respuesta.json() )
             .then( datos => {
-                switch(document.title.split("|")[1].trim()){
-                    case "Jugueteria":{
-                        this.disponibles =[...new Set( datos.map(e=>{
-                            if(e.categoria=="jugueteria"){
-                                return{
-                                    _id:e._id,
-                                    disponibles:e.disponibles
-        
-                                }
-                            }
-                        }))]
-                        this.disponibles.shift()
-                        this.tarjetas= datos.filter(e=>e.categoria=="jugueteria")
-                        this.categorias=datos.filter(e=>e.categoria=="jugueteria")
-                        this.tarjetasFiltradas = datos.filter(e=>e.categoria=="jugueteria")
-                        this.categoriasFiltradas = [ ...new Set( this.categorias.map( tar => tar.precio ) ) ]
-                        this.categoriasOrdenadasMenorAMayor = this.categoriasFiltradas.sort((a, b) => a -b)
-                        break
-                    }
-                    case "Farmacia":{
-                        this.disponibles =[...new Set( datos.map(e=>{
-                            if(e.categoria=="farmacia"){
-                        return{
-                            _id:e._id,
-                            disponibles:e.disponibles
-                                }
-                            }
-                        }))]
-                        this.tarjetas= datos.filter(e=>e.categoria=="farmacia")
-                        this.categorias=datos.filter(e=>e.categoria=="farmacia")
-                        this.tarjetasFiltradas = datos.filter(e=>e.categoria=="farmacia")
-                        this.categoriasFiltradas = [ ...new Set( this.categorias.map( tar => tar.precio ) ) ]
-                        this.categoriasOrdenadasMenorAMayor = this.categoriasFiltradas.sort((a, b) => a -b)
-                        break
-                    }
-                }
+                this.todosLosProductos=[...datos];
+                this.todosLosProductos.forEach(producto=>producto.ventas=0);
 
+                if(localStorage.getItem("nuestrosProductos"))
+                this.todosLosProductos = JSON.parse(localStorage.getItem("nuestrosProductos"));
+                let nombrePagina=location.pathname;//ve pagina nombre
+                if (nombrePagina.includes("farmacia"))
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="farmacia");
+                else
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="jugueteria");
+
+                this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+
+                this.productosFiltrados = this.productos;
             } )
-            .catch( )   
-            JSON.parse(localStorage.getItem("carrito"))
+            .catch()
+
+            
     },
     methods: {
-    filtroCruzado: function(){
-            let filtradoPorBusqueda = this.tarjetas.filter( eventos => eventos.producto.toLowerCase().includes( this.valorDeBusqueda.toLowerCase()))
+        filtroCruzado: function(){
+
+            console.log(this.perroOGtao)
+            let filtradoPorBusqueda = this.productos.filter( eventos => eventos.producto.toLowerCase().includes( this.valorDeBusqueda.toLowerCase()))
             if( this.chequeados.length === 0 ){
-                this.tarjetasFiltradas = filtradoPorBusqueda
+                this.productosFiltrados = filtradoPorBusqueda
             }else{
-                let filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados.includes( eventos.precio ))
-                this.tarjetasFiltradas = filtradosPorCheck 
+                let filtradosPorCheck
+                if(this.chequeados<=1500){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos =>this.chequeados>=eventos.precio)
+                }else if(this.chequeados==1501){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos => (this.chequeados<eventos.precio&&eventos.precio<=2000))
+                }
+                else if(this.chequeados==2001){
+                    filtradosPorCheck = filtradoPorBusqueda.filter( eventos => this.chequeados<=eventos.precio)
+                    
+                }
+                this.productosFiltrados = filtradosPorCheck 
             }
         },
-    agregar: function(objeto){
-            if(objeto.disponibles==0){
-                alert("no hay stock disponible")
-            }
-            if(objeto.disponibles>0){
-                this.carrito=this.carrito.concat(objeto)
-                objeto.disponibles--
-                localStorage.setItem("carrito",JSON.stringify(this.carrito))
-            }
-
+        agregar: function(objeto){//crear v-if en disponibles y boton agregar colocar mensaje no hay displonibles
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles--;
+                producto.ventas++;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            console.log(this.compras)
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
         },
         eliminar: function(objeto){
-            let filtro=this.disponibles.find(e=>e._id==objeto._id)
-            if(this.carrito.some(e=>e._id==objeto._id)){
-                this.aux=this.carrito.filter(e=>e._id==objeto._id)
-                if(objeto.disponibles<filtro.disponibles){
-                    this.aux.shift()
-                    objeto.disponibles++
-                }
-                if(this.aux.length>0){
-                this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
-                this.carrito=this.carrito.concat(this.aux)
-                }
-
-                else{
-                    this.carrito=this.carrito.filter(e=>e._id!=objeto._id)
-                }
-            }                
-            else if(this.carrito.length==0){
-                alert("no hay productos que eliminar en el carrito ")
-            }
-            else if(this.carrito.length>0){
-                alert("ese producto no se encuentra en el carrito")
-            }
-            localStorage.setItem("carrito",JSON.stringify(this.carrito))
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles++;
+                producto.ventas--;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
         },
         verMas: function(id){
-            this.tarjetas.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
+            this.todosLosProductos.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
+        },
+        // removerCarrito:function(){
+        // localStorage.removeItem("nuestrosProductos");
+        // this.productos=this.todos;
+        // this.compras=[];
+        // console.log(this.productos)
+        // },
+        submit() {
+
+            this.evaluarInputs(true);
+
+            if(!Object.values(this.errores).some(e => e !== "") || 
+                Object.entries(this.errores).filter(e => e[1] !== "").length === 1 && 
+                this.errores.otraMascota &&
+                this.data.mascota !== "otro" ){
+
+                this.enviando = true;
+
+                setTimeout(() => this.enviando = false, 2000)
+
+                setTimeout(() => {
+
+                    this.resetearDatos()
+                    this.enviando = null
+    
+                }, 6000)
+
+            }
+
+        },
+
+        resetearDatos(){
+
+            for(let [key, value] of Object.entries(this.data)){
+
+                this.data[key] = ""
+
+            }
+
+        },
+
+        evaluarInputs(isenviando){
+
+            for(let [key] of Object.entries(this.errores)){
+
+                if (isenviando && !this.data[key]) this.errores[key] = "*campo obligatorio";
+
+                else if(this.data[key]) this.errores[key] = "";
+
+                if(this.data[key]){
+
+                    if(!/^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g.test(this.data[key]) && 
+                        (   key === "nombre" || 
+                            key === "apellido" || 
+                            key === "OtraMascota")) this.errores[key] = "*sólo introducir letras";
+    
+                    else if(key === "telefono" && 
+                            !/^[0-9]+$/g.test(this.data[key])) this.errores.telefono = "*sólo introducir números";
+
+                }
+
+            }
+
         }
     },
     computed: {
 
         cargando(){
 
-            if(!this.tarjetas.length) setTimeout(() => this.estaCargando = false, 2000)
+            if(this.productosFiltrados.length) setTimeout(() => this.estaCargando = false, 2000)
         
+        },
+        reEvaluarInputs() {
+
+            this.evaluarInputs(false);
+
         }
 
-    }
 
-} ).mount("#app")
+    } 
+    }).mount("#app")
