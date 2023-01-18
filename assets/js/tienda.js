6ef44e0e4682597c48e23bd41680141f614cfc6e
darthenis <emi.acevedo.letras@gmail.com>
2023-01-17 23:09:28 -0300

    update

--- assets/js/tienda.js
+++ assets/js/tienda.js
@@ -1,6 +1,74 @@
 const { createApp } = Vue
 createApp( {
-    
+    data(){
+        return {
+            todosLosProductos: [],
+            compras: [],
+            productos:[],
+            informacionDeTarjeta:{},
+            chequeados:[],
+            valorDeBusqueda:"",
+            todos:[],
+        }
+    },
+    created(){
+        fetch(`https://mindhub-xj03.onrender.com/api/petshop`)
+            .then( respuesta => respuesta.json() )
+            .then( datos => {
+                this.todosLosProductos=[...datos];
+                
+                this.todosLosProductos.forEach(producto=>producto.ventas=0);
+
+                if(localStorage.getItem("nuestrosProductos"))
+                this.todosLosProductos = JSON.parse(localStorage.getItem("nuestrosProductos"));
+                let nombrePagina=location.pathname;//ve pagina nombre
+                if (nombrePagina.includes("farmacia"))
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="farmacia");
+                else
+                this.productos=this.todosLosProductos.filter(producto=>producto.categoria==="jugueteria");
+
+                this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            } )
+            .catch()
+    },
+    methods: {
+        filtroCruzado: function(){
+            let filtradoPorBusqueda = this.productos.filter( eventos => eventos.producto.toLowerCase().includes( this.valorDeBusqueda.toLowerCase()))
+            if( this.chequeados.length === 0 ){
+                this.productosFiltrados = filtradoPorBusqueda
+            }else{
+                
+            }
+        },
+        agregar: function(objeto){//crear v-if en disponibles y boton agregar colocar mensaje no hay displonibles
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles--;
+                producto.ventas++;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            console.log(this.compras)
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
+        },
+        eliminar: function(objeto){
+            this.todosLosProductos.find(producto=>{
+                if(producto._id==objeto._id){
+                producto.disponibles++;
+                producto.ventas--;}
+            });
+            this.compras=this.todosLosProductos.filter(e=>e.ventas>0);
+            localStorage.setItem("nuestrosProductos",JSON.stringify(this.todosLosProductos));
+        },
+        verMas: function(id){
+            this.todosLosProductos.forEach(tarjeta => tarjeta._id === id ? this.informacionDeTarjeta = tarjeta : `No hay informacion acerca del producto` );
+        },
+        removerCarrito:function(){
+        localStorage.removeItem("nuestrosProductos");
+        this.productos=this.todos;
+        this.compras=[];
+        console.log(this.productos)
+        },
+    },
     computed: {
 
         cargando(){
